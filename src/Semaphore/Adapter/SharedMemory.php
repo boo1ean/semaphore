f90b5c0e7441dd5e0ef75ba1f11af9603876f2a9
boo1ean <boo1ean0807@gmail.com>
2013-12-10 22:04:49 +0200

    Implement shared memory adapter

---
+++ src/Semaphore/Adapter/SharedMemory.php
@@ -0,0 +1,143 @@
+<?php namespace Semaphore\Adapter;
+
+use RuntimeException;
+use Shared\Storage;
+
+class SharedMemory implements AdapterInterface
+{
+	/**
+	 * Key for semaphores counter in shm
+	 * @const string
+	 */
+	const K_SEM_COUNT = 'semaphores count';
+
+	/**
+	 * Key for semaphores list in shm
+	 * @const string
+	 */
+	const K_SEM_LIST = 'semaphores list';
+
+	/**
+ 	 * Shared memory storage
+ 	 * @var Shared\Storage
+	 */
+	protected $shm;
+
+	/**
+	 * Default shm key, unique enough
+	 * @var string
+	 */
+	const KEY = 'very_unique_shared_memory_key_really...';
+
+	/**
+	 * Create shared memory adapter with given key
+	 * @param string $key
+	 */
+	public function __construct($key = self::KEY) {
+		$this->key = $key;
+		$this->setupStorage();
+	}
+
+	/**
+	 * Lock semaphore with given key
+	 * @param string $key
+	 * @return bool
+	 */
+	public function lock($key) {
+		if ($this->locked($key)) {
+			return false;
+		}
+
+		return $this->_lock($key, true);
+	}
+
+	/**
+	 * Unlock semaphore with given key
+	 * @param string $key
+	 * @return bool
+	 */
+	public function unlock($key) {
+		if (!$this->locked($key)) {
+			return false;
+		}
+
+		return $this->_unlock($key);
+	}
+
+	/**
+	 * Check if semaphore with given key are locked
+	 * @param string $key
+	 * @return bool
+	 */
+	public function locked($key) {
+		return $this->shm->has($key);
+	}
+
+	/**
+	 * Create semaphore key from storage
+	 * @param string $key
+	 * @return bool
+	 */
+	protected function _lock($key) {
+		$result = $this->shm->set($key, true);
+
+		if ($result) {
+			$this->incCounter();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Remove semaphore key from storage
+	 * @param string $key
+	 * @return bool
+	 */
+	protected function _unlock($key) {
+		$result = $this->shm->unset($key);
+
+		if ($result) {
+			$this->decCounter();
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Increment semaphores counter
+	 * @return int
+	 */
+	protected function incCounter() {
+		$counter = $this->shm->get(self::K_SEM_COUNT);
+		return $this->shm->set(self::K_SEM_COUNT, $counter + 1);
+	}
+
+	/**
+	 * Decrement semaphores counter
+	 * @return int
+	 */
+	protected function decCounter() {
+		$counter = $this->shm->get(self::K_SEM_COUNT);
+		return $this->shm->set(self::K_SEM_COUNT, $counter - 1);
+	}
+
+	/**
+	 * Setup shared memory storage
+	 */
+	protected function setupStorage() {
+		$this->shm = new Storage($this->key);
+		$count = $this->shm->get(self::K_SEM_COUNT);
+
+		if (is_null($count)) {
+			$this->initEmptyStorage();
+		}
+	}
+
+	/**
+	 * Initialize storage with empty values
+	 */
+	protected function initEmptyStorage() {
+		$this->shm->set(self::K_SEM_COUNT, 0);
+		$this->shm->set(self::K_SEM_LIST, array());
+	}
+}
